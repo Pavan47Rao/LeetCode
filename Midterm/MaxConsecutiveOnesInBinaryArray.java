b38d71001710ef26990c8a77d0200ffe7a65a095
Pavan Rao <pavan47kalyan@gmail.com>
2021-04-01 21:52:12 -0400

    Added max consecutive ones in binary array prob

--- Midterm/MaxConsecutiveOnesInBinaryArray.java
+++ Midterm/MaxConsecutiveOnesInBinaryArray.java
@@ -1,7 +1,12 @@
 package Midterm;
 
+/**
+ * Time complexity = O(n)
+ * Space complexity = O(1)
+ */
+
 public class MaxConsecutiveOnesInBinaryArray {
-    public int findMaxConsecutiveOnes(int[] nums) {
+    public static int findMaxConsecutiveOnes(int[] nums) {
         int maxCount = 0, count = 0;
         for(int i=0; i< nums.length;i++) {
             if(nums[i] == 1) {
@@ -12,5 +17,16 @@ public class MaxConsecutiveOnesInBinaryArray {
                 count = 0;
         }
         return maxCount;
+    }
+
+    public static void main(String args[]) {
+        int[] nums = {0,1,1,0,1,1,1,1,0};
+        System.out.println(findMaxConsecutiveOnes(nums));
+        int[] nums2 = {1};
+        System.out.println(findMaxConsecutiveOnes(nums2));
+        int[] nums3 = {0};
+        System.out.println(findMaxConsecutiveOnes(nums3));
+        int[] nums4 = {1,1,1,1,1,1,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums4));
     }
 }

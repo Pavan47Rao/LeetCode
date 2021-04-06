4b7d747968099f2a32a704af4afd770786df7935
Pavan Rao <pavan47kalyan@gmail.com>
2021-04-06 04:39:27 -0400

    Added combination sum prob

---
+++ Medium/CombinationSum.java
@@ -0,0 +1,72 @@
+/**
+ * Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+Example 4:
+
+Input: candidates = [1], target = 1
+Output: [[1]]
+Example 5:
+
+Input: candidates = [1], target = 2
+Output: [[1,1]]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+All elements of candidates are distinct.
+1 <= target <= 500
+
+tc: O(N^((T/M)+1)) N-no of candidates, T - target, M - Minimal value among candidates
+sc: O(T/M)
+ */
+
+package Medium;
+import java.util.*;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        LinkedList<Integer> combination = new LinkedList<>();
+        combinationSum(candidates, target, combination, 0, result);
+        return result;
+    }
+    
+    public void combinationSum(int[] candidates, int remainingTarget, LinkedList<Integer> combination, int start, List<List<Integer>> result) {
+        if(remainingTarget == 0) {
+            result.add(new ArrayList<Integer>(combination));
+            return;
+        }
+        else if(remainingTarget < 0) {
+            return;
+        }
+        for(int i = start; i < candidates.length; i++) {
+            combination.add(candidates[i]);
+            combinationSum(candidates, remainingTarget - candidates[i], combination, i, result);
+            combination.removeLast();
+        }
+    }
+}

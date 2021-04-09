c464b360fedb138dc86c6038accee8a6bf8a22e0
Pavan Rao <pavan47kalyan@gmail.com>
2021-04-09 17:40:01 -0400

    Added second approch for search in 2d matrix prob

--- Medium/SearchATwoDMatrix.java
+++ Medium/SearchATwoDMatrix.java
@@ -72,3 +72,27 @@ public class SearchATwoDMatrix {
         return matrix[targetRow][targetCol] == target;
     }
 }
+
+/**
+ * Approach 2
+ * 
+ * public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = (m * n) - 1;
+        if(m == 0)
+            return false;
+        while(left <= right) {
+            int mid = (left + right)/2;
+            int midElement = matrix[mid/n][mid%n];
+            if(target == midElement)
+                return true;
+            else if(target < midElement)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return false;
+    }
+ */

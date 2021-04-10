e5b0e133655a3c710b2498ff87371b9e453d5462
Pavan Rao <pavan47kalyan@gmail.com>
2021-04-09 20:34:03 -0400

    Added iterative approach for binary tree inorder traversal

--- Medium/BinaryTreeeInorderTraversal.java
+++ Medium/BinaryTreeeInorderTraversal.java
@@ -58,3 +58,22 @@ class TreeNode {
         this.right = right;
     }
 }
+
+/**
+ * Iterative approach
+ * public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+        return result;
+    }
+ */

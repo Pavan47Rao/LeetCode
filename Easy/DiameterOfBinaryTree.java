29f1f370c53b73da1825c9c420914edd71c9d1bf
Pavan Rao <pavan47kalyan@gmail.com>
2021-04-12 05:38:55 -0400

    Added diameter of binary tree prob

---
+++ Easy/DiameterOfBinaryTree.java
@@ -0,0 +1,44 @@
+/**
+ * Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+ */
+
+package Easy;
+
+public class DiameterOfBinaryTree {
+    int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        diameter(root);
+        return diameter;
+    }
+    
+    public int diameter(TreeNode node) {
+        if(node == null)
+            return 0;
+        int leftPath = diameter(node.left);
+        int rightPath = diameter(node.right);
+        diameter = Math.max(diameter, leftPath + rightPath);
+        return Math.max(leftPath, rightPath) + 1;
+    }
+}

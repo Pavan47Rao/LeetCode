100ad1aaf83c4bc935eedb648c6304567703afcf
Pavan Rao <pavan47kalyan@gmail.com>
2021-05-27 21:20:10 -0400

    Added optimized approach to paint house 2 prob

--- Hard/PaintHouse2.java
+++ Hard/PaintHouse2.java
@@ -54,3 +54,26 @@ public class PaintHouse2{
         return minPrice;
     }
 }
+
+/**
+Above approach has O(n*k*k) complexity, the following has O(n*k)
+
+for(int house = 1; house < n; house++) {
+    int minColor = -1, secondMinColor = -1;
+    for(int color = 0; color < k; color++) {
+        int cost = costs[house-1][color];
+        if(minColor == -1 || cost < costs[house-1][minColor]) {
+            secondMinColor = minColor;
+            minColor = color;
+        }
+        else if(secondMinColor == -1 || cost < costs[house-1][secondMinColor])
+            secondMinColor = color;
+    } 
+    for(int color = 0; color < k; color++) {
+        if(color == minColor)
+            costs[house][color] += costs[house-1][secondMinColor];
+        else
+            costs[house][color] += costs[house-1][minColor];
+    }
+}
+ */

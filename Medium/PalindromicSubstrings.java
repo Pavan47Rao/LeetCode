c127c18aaa8e40081e5ed2a29a87168d848730eb
Pavan Rao <pavan47kalyan@gmail.com>
2021-06-05 21:41:10 -0400

    Added palindromic substrings prob

---
+++ Medium/PalindromicSubstrings.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ * Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+
+ */
+
+package Medium; 
+
+public class PalindromicSubstrings {
+    public int countSubstrings(String s) {
+        int n = s.length(), result = 0;
+        if(n <= 0)
+            return 0;
+        boolean[][] dp = new boolean[n][n];
+        
+        for(int i = 0; i < n; i++, result++) {
+            dp[i][i] = true;
+        }
+        
+        for(int i = 0; i < n-1; i++) {
+            dp[i][i+1] = s.charAt(i) == s.charAt(i+1);
+            result += dp[i][i+1] ? 1 : 0;
+        }
+        
+        for(int len = 3; len <= n; len++) {
+            for(int i = 0, j = i + len -1; j < n; i++, j++) {
+                dp[i][j] = dp[i+1][j-1] && (s.charAt(i) == s.charAt(j));
+                result += dp[i][j] ? 1 : 0;
+            }
+        }
+        
+        return result;
+    }
+}

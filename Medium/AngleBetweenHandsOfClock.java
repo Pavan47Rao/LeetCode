4db980976a3015d429a042739e5c4a65a5c3690e
Pavan Rao <pavan47kalyan@gmail.com>
2021-06-23 13:41:36 -0400

    Added angle between hands of clock prob

---
+++ Medium/AngleBetweenHandsOfClock.java
@@ -0,0 +1,52 @@
+/**
+ * Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand.
+
+ 
+
+Example 1:
+
+
+
+Input: hour = 12, minutes = 30
+Output: 165
+Example 2:
+
+
+
+Input: hour = 3, minutes = 30
+Output: 75
+Example 3:
+
+
+
+Input: hour = 3, minutes = 15
+Output: 7.5
+Example 4:
+
+Input: hour = 4, minutes = 50
+Output: 155
+Example 5:
+
+Input: hour = 12, minutes = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= hour <= 12
+0 <= minutes <= 59
+Answers within 10^-5 of the actual value will be accepted as correct.
+ */
+
+package Medium;
+
+public class AngleBetweenHandsOfClock {
+    public double angleClock(int hour, int minutes) {
+        int oneMinAngle = 6;
+        int oneHourAngle = 30;
+        double minAngle = minutes * oneMinAngle;
+        double hourAngle = (hour % 12 + minutes/60.0) * oneHourAngle;
+        double diff = Math.abs(minAngle - hourAngle);
+        return Math.min(diff, 360 - diff);
+    }
+}

d193c8d639dd64e75568d4720c981c03f0620162
Pavan Rao <pavan47kalyan@gmail.com>
2021-06-23 16:21:33 -0400

    Added binary search approach to find k closest elements prob

--- Medium/FindKClosestElements.java
+++ Medium/FindKClosestElements.java
@@ -40,3 +40,21 @@ public class FindKClosestElements {
         return res;
     }
 }
+
+/**
+ * Binary search approach:
+ * public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0, right = arr.length - k;
+        while(left < right) {
+            int mid = (left + right) / 2;
+            if(x-arr[mid] > arr[mid+k]-x)
+                left = mid+1;
+            else
+                right = mid;
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i = left; i < left+k; i++)
+            result.add(arr[i]);
+        return result;
+    }
+ */

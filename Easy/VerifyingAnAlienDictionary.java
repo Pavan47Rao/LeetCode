7abbb850d2f18d31069b1f21e5fc32b3f8f53364
Pavan Rao <pavan47kalyan@gmail.com>
2021-07-22 09:53:52 -0400

    Corrected verifying alien dictionary prob

--- Easy/VerifyingAnAlienDictionary.java
+++ Easy/VerifyingAnAlienDictionary.java
@@ -39,7 +39,7 @@ public class VerifyingAnAlienDictionary {
         for(int i = 0; i < order.length(); i++)
             orderMap[order.charAt(i) - 'a'] = i;
         for(int i = 0; i < words.length-1; i++) {
-            for(int j = 0; j < words[0].length(); j++) {
+            for(int j = 0; j < words[i].length(); j++) {
                 if(j >= words[i+1].length())
                     return false;
                 if(words[i].charAt(j) != words[i+1].charAt(j)) {

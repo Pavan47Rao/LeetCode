e1338053921360b997eeb294b6405e624fbb3e54
Pavan Rao <pavan47kalyan@gmail.com>
2021-07-25 21:39:17 -0400

    Added bst approach to closest bst val prob

--- Easy/ClosestBinarySearchTreeValue.java
+++ Easy/ClosestBinarySearchTreeValue.java
@@ -42,3 +42,18 @@ public class ClosestBinarySearchTreeValue {
         return arr[1].intValue();
     }
 }
+
+/**
+ * Second approach
+ * 
+ * public int closestValue(TreeNode root, double target) {
+ *      int val, closest = root.val;
+ *      while(root != null) {
+ *          val = root.val;
+ *          closest = Math.abs(val - target) > Math.abs(closest - target) ? val : closest;
+ *          root = target < root.val ? root.left : root.right;
+ *      }
+ *      return closest;
+ * }
+ * 
+ */
